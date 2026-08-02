fa4efc7cc8c48ac7ffd4fa83cb4e5d10a466483d
Devaruban U <udevaruban@gmail.com>
2026-08-03 01:38:46 +0530

    Updated portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import { useState } from "react";
 import Navbar from "./Components/Navbar";
-import { HashRouter } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Skills from "./Components/Skills";
-import Project from "./Components/Projects";
 import Contact from "./Components/Contact";
-import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
-import Qualification from "./Components/Qualification";
-import Projects from "./Components/Projects";
+import { BrowserRouter as Routers, Routes, Route, HashRouter } from "react-router-dom";
 import Achievements from "./Components/Achievements";
 import EventOrganized from "./Components/EventOrganized";
+import Projects from "./Components/Projects";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div
-      className={`min-h-screen ${darkMode ? "bg-gray-900 text-white" : "bg-white text-gray-900"}`}
-    >
+    <div className={`min-h-screen ${darkMode ? "bg-gray-900 text-white" : "bg-white text-gray-900"}`}>
       <HashRouter>
         <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
         <Routes>
@@ -30,7 +25,7 @@ const App = () => {
           <Route path="/achievements" element={<Achievements />} />
           <Route path="/event" element={<EventOrganized />} />
         </Routes>
-      <HashRouter>
+      </HashRouter>
     </div>
   );
 };
